c7e44ea616170c0eadd243e39d774fe51260cd11
Konstantin Cherkashin <tcherkashin94@gmail.com>
2022-05-21 18:34:25 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,20 +4,11 @@ import PackageDescription
 
 let package = Package(
     name: "FeedKit",
-    platforms: [
-        .macOS(.v10_12),
-        .iOS(.v10),
-        .tvOS(.v10),
-        .watchOS(.v3)
-    ],
     products: [
         .library(name: "FeedKit", targets: ["FeedKit"]),
     ],
     targets: [
         .target(name: "FeedKit", dependencies: []),
         .testTarget(name: "Tests", dependencies: ["FeedKit"], path: "Tests")
-    ],
-    swiftLanguageVersions: [
-        .v5
     ]
 )
